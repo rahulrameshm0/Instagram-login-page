2f559e86856c00d1a42f3131ae1bed2c7c1cfeea
rahulrameshm0 <iamrahulm23@gmail.com>
2025-03-09 16:12:06 +0530

    updated code

--- instagram/instagram/wsgi.py
+++ instagram/instagram/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from instagram.wsgi import application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
 
 application = get_wsgi_application()
